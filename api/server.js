94c185c5f8e46ed8f4ccb11184e28f05aa67eba5
unknown <everest.k6@gmail.com>
2020-04-08 19:48:23 -0700

    Initial Commit

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const PostRouter = require("../posts/post-router.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/posts", PostRouter);
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Error retrieving the data from Server"
+  });
+})
 
 module.exports = server;
